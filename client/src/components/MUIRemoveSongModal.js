4c53b099571c52c92e71e4bef1080b2320a4c591
dlian7890 <dongpinglian7890@gmail.com>
2022-11-02 22:03:06 -0400

    Redesigned delete song modal using MaterialUI

--- client/src/components/MUIRemoveSongModal.js
+++ client/src/components/MUIRemoveSongModal.js
@@ -2,14 +2,17 @@ import { useContext } from 'react';
 import GlobalStoreContext from '../store';
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 500,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -38,8 +41,31 @@ export default function MUIRemoveSongModal() {
 
   return (
     <Modal open={store.currentSong !== null}>
-      {/* <Box sx={style}>
-        <div
+      <Box sx={style}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography component='h1' variant='h4'>
+              Remove {songTitle}?
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Box
+              component='span'
+              m={1}
+              display='flex'
+              justifyContent='space-between'
+              alignItems='center'
+            >
+              <Button variant='contained' onClick={handleConfirmRemoveSong}>
+                Confirm
+              </Button>{' '}
+              <Button variant='contained' onClick={handleCancelRemoveSong}>
+                Cancel
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+        {/* <div
           id='remove-song-modal'
           className={modalClass}
           data-animation='slideInOutLeft'
@@ -69,8 +95,8 @@ export default function MUIRemoveSongModal() {
               />
             </div>
           </div>
-        </div>
-      </Box> */}
+        </div> */}
+      </Box>
     </Modal>
   );
 }
