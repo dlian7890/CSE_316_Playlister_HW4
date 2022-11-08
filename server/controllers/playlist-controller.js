e90383f6d9d2c559324b8e8f09e679ca42a61705
dlian7890 <dongpinglian7890@gmail.com>
2022-11-07 20:54:57 -0500

    Fixed playlist not removed from user document when playlist is deleted.

--- server/controllers/playlist-controller.js
+++ server/controllers/playlist-controller.js
@@ -61,9 +61,14 @@ deletePlaylist = async (req, res) => {
         console.log('req.userId: ' + req.userId);
         if (user._id == req.userId) {
           console.log('correct user!');
-          Playlist.findOneAndDelete({ _id: req.params.id }, () => {
-            return res.status(200).json({});
-          }).catch((err) => console.log(err));
+          user.playlists = user.playlists.filter((list) => {
+            return JSON.stringify(list) != JSON.stringify(playlist._id);
+          });
+          user.save().then(() => {
+            Playlist.findOneAndDelete({ _id: req.params.id }, () => {
+              return res.status(200).json({});
+            }).catch((err) => console.log(err));
+          });
         } else {
           console.log('incorrect user!');
           return res.status(400).json({
