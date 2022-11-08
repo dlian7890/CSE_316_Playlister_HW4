5c880a230dd22a6d8a1e4fcd7cdbfa23d64a2a1c
dlian7890 <dongpinglian7890@gmail.com>
2022-11-07 20:58:51 -0500

    Fixed user not directed to home screen after a successful register.

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -102,7 +102,7 @@ function AuthContextProvider(props) {
             user: response.data.user,
           },
         });
-        // history.push('/login');
+        history.push('/');
       }
     } catch (err) {
       auth.setErrorMessage(err.response.data.errorMessage);
